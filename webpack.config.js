f944415a8a181aaa5a17e191051d5e1e5fcefc95
Nishant <nishant.kumar.shreevastav@gmail.com>
2018-12-13 22:31:15 +0530

    webpack config composition

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const console = require("better-console");
+// const util = require("util");  // uncomment for debugging
 const merge = require('webpack-merge');
 const parts = require('./webpack.parts');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -12,12 +12,17 @@ const commonConfig = merge([{
     module : {
         rules : [
             {
-                test : "/\.js/",
-                exclude : "/node_modules/",
+                test : /\.js$/,
+                exclude : /node_modules/,
                 use : {
 
-                    loader : "babel-loader"
+                    loader : 'babel-loader',
                 }
+            },
+            {
+                test : /\.scss/,
+                exclude : /node_modules/,
+                use : ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
@@ -33,38 +38,20 @@ const prodConfig = merge([]);
 
 module.exports = mode => {
     if(mode === "production"){
-        let config = merge( commonConfig, prodConfig, mode);
-        console.log("production configurations " , config.module.rules);
+    /* below code is used for debugging prupose     
+        let config = merge( commonConfig, prodConfig, {mode});
+        console.log("production configurations ", seperator);
+        console.log(util.inspect(config, { showHidden : false, depth : null}));
         return config;
+    */
     }
-    let config = merge (commonConfig, devConfig, mode);
-    console.log("development configurations ", config.module.rules);
+    return merge(commonConfig, devConfig, {mode})
+    /* below code is used for debugging purpose
+    let config = merge (commonConfig, devConfig, {mode});
+    console.log("production configurations ", seperator);
+    console.log(util.inspect(config, {showHidden : false , depth : null}));
     return config;
+    */
 
 }
-/*module.exports = {
-    devServer : {
-        stats : "errors-only",
-        host : process.env.HOST,
-        port : process.env.PORT,
-        open : true,
-        overlay : true
-    },
-    module : {
-        rules : [
-            {
-                test : /\.js$/,
-                exclude : /node_modules/,
-                use : {
-                    loader : "babel-loader"
-                }
-            },
-            {
-                test : /\.scss$/,
-                exclude : /node_modules/,
-                use : ["style-loader" , "css-loader" , "sass-loader"]
-            }
-        ],
-    },
-    plugins : [ htmlPlugin ]
-}*/
+
